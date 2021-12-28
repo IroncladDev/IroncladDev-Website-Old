173f35964a6785ac1b52bad9cc08da3fa051dcb0
Conner Ow <connerow1115@gmail.com>
2021-12-27 19:03:22 -0600

    fix api route

--- pages/api/email.js
+++ pages/api/email.js
@@ -3,19 +3,21 @@ import { app, sendEmail } from '../../scripts/util.js'
 
 
 
-app.post(async (req, res) => {
+app.post((req, res) => {
   if (req.method === "POST") {
     if (req.body.auth === process.env.ADMSS) {
       try {
-        let subs = await Sub.find({})
-        await (function(){
-          for(var i = 0; i < subs.length; i++){
-            sendEmail(subs[i].email, req.body.title, req.body.body.replace(/\n/g, "<br>") + "<br><br>If these emails aren't worth your time, simply click <a href='"+"https://"+req.headers.host+"/api/removeme?email="+subs[i].email+"'>this link</a> to unsubscribe")
+        Sub.find({}, (err, subs) => {
+          if (err) {
+            res.status(500).json({ success: false, error: err })
+          } else {
+            for (var i = 0; i < subs.length; i++) {
+              sendEmail(subs[i].email, req.body.title, req.body.body.replace(/\n/g, "<br>") + "<br><br>If these emails aren't worth your time, simply click <a href='" + "https://" + req.headers.host + "/api/removeme?email=" + subs[i].email + "'>this link</a> to unsubscribe")
+            }
+            res.status(200).json({ success: true, data: subs });
           }
-        })();
-        await res.status(200).json({ success: true, data: subs });
+        })
       } catch (err) {
-        console.log(err);
         res.status(500).json({ success: false, error: err })
       }
     } else {
