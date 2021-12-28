3a5da96dbe179996557a235f77304a35661a684c
Conner Ow <connerow1115@gmail.com>
2021-12-27 18:37:09 -0600

    show subs

--- pages/api/email.js
+++ pages/api/email.js
@@ -11,7 +11,7 @@ app.post(async (req, res) => {
         for(var i = 0; i < subs.length; i++){
           sendEmail(subs[i].email, req.body.title, req.body.body.replace(/\n/g, "<br>") + "<br><br>If these emails aren't worth your time, simply click <a href='"+"https://"+req.headers.host+"/api/removeme?email="+subs[i].email+"'>this link</a> to unsubscribe")
         }
-        await res.status(200).json({ success: true });
+        await res.status(200).json({ success: true, data: subs });
       } catch (err) {
         console.log(err);
         res.status(500).json({ success: false, error: err })
