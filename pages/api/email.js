b90417d2affcecf3454c4e0ca7bb5b92f25d13d6
Conner Ow <connerow1115@gmail.com>
2021-12-27 18:58:26 -0600

    await email sending

--- pages/api/email.js
+++ pages/api/email.js
@@ -8,9 +8,11 @@ app.post(async (req, res) => {
     if (req.body.auth === process.env.ADMSS) {
       try {
         let subs = await Sub.find({})
-        for(var i = 0; i < subs.length; i++){
-          sendEmail(subs[i].email, req.body.title, req.body.body.replace(/\n/g, "<br>") + "<br><br>If these emails aren't worth your time, simply click <a href='"+"https://"+req.headers.host+"/api/removeme?email="+subs[i].email+"'>this link</a> to unsubscribe")
-        }
+        await (function(){
+          for(var i = 0; i < subs.length; i++){
+            sendEmail(subs[i].email, req.body.title, req.body.body.replace(/\n/g, "<br>") + "<br><br>If these emails aren't worth your time, simply click <a href='"+"https://"+req.headers.host+"/api/removeme?email="+subs[i].email+"'>this link</a> to unsubscribe")
+          }
+        })();
         await res.status(200).json({ success: true, data: subs });
       } catch (err) {
         console.log(err);
