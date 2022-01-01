8955445c949621bde381bfc6480d333456e418df
Conner Ow <connerow1115@gmail.com>
2022-01-01 07:19:11 -0600

    open blog posts in new tab

--- components/bloog.js
+++ components/bloog.js
@@ -3,13 +3,13 @@ import styles from '../styles/blog.module.css'
 import Link from 'next/link'
 export default function Post(props) {
   return (
-    <Link href={props.url} passHref>
+    <a href={props.url} target="_blank" rel="noreferrer">
     <div className={styles.post}>
       <img src={props.cover} className={styles.cover} alt={props.title}/>
       <div className={styles.dateMarker}>{props.date}, {props.year} • {props.read} min read</div>
       <h2 className={styles.title}>{props.title}</h2>
       <p className={styles.desc}>{props.desc}</p>
     </div>
-    </Link>
+    </a>
   );
 }
